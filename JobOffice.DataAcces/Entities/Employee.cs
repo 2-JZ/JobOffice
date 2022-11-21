eb9f7f53558ce43f5deda575f470f4bd3449d6d3
Tomasz Kowalczyk <TomaszKowalczykAGH@gmail.com>
2022-11-21 13:18:57 +0100

    ValidationInEmployee migration, updated Employee

--- JobOffice.DataAcces/Entities/Employee.cs
+++ JobOffice.DataAcces/Entities/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,9 +8,13 @@ using System.Threading.Tasks;
 namespace JobOffice.DataAcces.Entities
 {
     public class Employee: EntityBase
-    {
+    {   
+        [Required]
+        public string? Name { get; set; }
         
+        [MaxLength(100)]
         public string? Surname { get; set; }
-        public int ContactID { get; set; }
+        
+        public int? ContactID { get; set; }
     }
 }
