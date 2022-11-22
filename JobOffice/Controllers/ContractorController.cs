c5c6eb9b283fba28384f43f401ef47aa703d376b
Tomasz Kowalczyk <TomaszKowalczykAGH@gmail.com>
2022-11-22 15:13:11 +0100

    Repaired Connection String

--- JobOffice/Controllers/ContractorController.cs
+++ JobOffice/Controllers/ContractorController.cs
@@ -1,6 +1,23 @@
-﻿namespace JobOffice.Controllers
+﻿using JobOffice.DataAcces;
+using JobOffice.DataAcces.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobOffice.Controllers
 {
-    public class ContractorController
-    {
+    [ApiController]
+    [Route("[controller]")]
+    public class ContractorController : ControllerBase
+    { 
+
+        private readonly IRepository<Contractor> contractorRepository;
+        public ContractorController(IRepository<Contractor> contractorRepository)
+        {
+            this.contractorRepository=contractorRepository;
+
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<Contractor> GetAllContractors() => this.contractorRepository.GetAll();
     }
 }
--- JobOffice/Program.cs
+++ JobOffice/Program.cs
@@ -2,15 +2,21 @@ using JobOffice.DataAcces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+string connectionString = builder.Configuration.GetConnectionString("JobOfficeConnection");
+builder.Services.AddDbContext<JobOfficeContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("JobOfficeConnection"));
 
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<JobOfficeContext>
-    (opt => opt.UseSqlServer(connectionString: "JobOfficeDatabase"));
+//builder.Services.AddDbContext<JobOfficeContext>
+//    (opt => opt.UseSqlServer(connectionString: "JobOfficeConnection"));
+
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 
