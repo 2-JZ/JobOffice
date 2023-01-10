6dbdb54cd702826b0932bdecb4c99be1504cfe24
Tomasz Kowalczyk <TomaszKowalczykAGH@gmail.com>
2023-01-10 20:42:31 +0100

    added Nlogger, and component CurrencyNbpConnector - GIT test --am

---
+++ JobOffice.ApplicationServices/Components/NBPWeb/Currency.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace JobOffice.ApplicationServices.Components.NBPWeb
+{
+    public class Currency
+    {
+        //public string CurrencyName { get; set; }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+        //public string Code { get; set; }
+
+        [JsonProperty("rates")]
+        public RatesData Rates { get; set; }
+    }
+}
