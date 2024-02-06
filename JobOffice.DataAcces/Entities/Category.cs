9748fb5b929cab0501290b0b7fe78c07287cd48f
2-JZ <tomaskowalcz@gmail.com>
2024-02-06 12:32:01 +0100

    Subcategory instead of maincategory

--- JobOffice.DataAcces/Entities/Category.cs
+++ JobOffice.DataAcces/Entities/Category.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace JobOffice.DataAcces.Entities
 {
-    internal class Category
+    public class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public string CategoryURL { get; set;}
+        public int IdSubCategory { get; set; }
+        public bool isActive { get; set;}
+        public DateTime CategoryCategoryId { get; set;} = DateTime.Now;
+        public int CategoryCategoryNameId { get;}
+
     }
 }
