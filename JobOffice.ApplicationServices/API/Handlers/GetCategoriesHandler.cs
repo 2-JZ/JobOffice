6344acfec0cb761c150a8146d16e491fa710a9d7
2-JZ <tomaskowalcz@gmail.com>
2024-02-06 01:08:10 +0100

    Updated Categories handler

--- JobOffice.ApplicationServices/API/Handlers/GetCategoriesHandler.cs
+++ JobOffice.ApplicationServices/API/Handlers/GetCategoriesHandler.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using JobOffice.ApplicationServices.API.Domain;
+using JobOffice.ApplicationServices.API.Domain.ErrorHandling;
+using JobOffice.ApplicationServices.API.Domain.Models;
+using JobOffice.DataAcces.CQRS;
+using JobOffice.DataAcces.CQRS.Queries;
+using MediatR;
 
 namespace JobOffice.ApplicationServices.API.Handlers
 {
-    internal class GetCategoriesHandler
+    public class GetCategoriesHandler : IRequestHandler<GetCategoriesRequest, GetCategoriesResponse>
     {
+        IQueryExecutor queryExecutor;
+        IMapper mapper;
+        public GetCategoriesHandler(IQueryExecutor queryExecutor, IMapper mapper)
+        {
+            this.queryExecutor = queryExecutor;
+            this.mapper = mapper;
+        }
+        public async Task<GetCategoriesResponse> Handle(GetCategoriesRequest request, CancellationToken cancellationToken)
+        {
+            if (request.AuthenticationRole.ToString() == "Developer")
+            {
+                return new GetCategoriesResponse()
+                {
+                    Error = new ErrorModel(ErrorType.Unauthorized)
+                };
+            }
+            else
+            {
+                var query = new GetCategoriesQuery();
+                var categoriesFromDb = await this.queryExecutor.Execute(query);
+                return new GetCategoriesResponse()
+                {
+                    Data = this.mapper.Map<List<Category>>(categoriesFromDb)
+                };
+            }
+        }
     }
 }
