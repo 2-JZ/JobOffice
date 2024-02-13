ee37c8f01b05471683258b3e8b23be14725c17d8
2-JZ <tomaskowalcz@gmail.com>
2024-02-13 17:17:46 +0100

    updated entities

--- JobOffice.ApplicationServices/API/Domain/Models/Category.cs
+++ JobOffice.ApplicationServices/API/Domain/Models/Category.cs
@@ -5,13 +5,22 @@ namespace JobOffice.ApplicationServices.API.Domain.Models
 {
     public class Category
     {
-        public string Name { get; set; } 
-        public string Description { get; set; }
-        public string Picture { get; set; }
-        public string CategoryURL { get; set; }
-        public int IdSubCategory { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Picture { get; set; }
+
+        public string? CategoryURL { get; set; }
+
+        public int? IdSubCategory { get; set; }
+
         public bool isActive { get; set; }
+
         public DateTime CreatedCategoryId { get; set; } = DateTime.Now;
-        public ICollection<SubCategory> SubCategories { get; set; }
+
+        public ICollection<SubCategory>? SubCategories { get; set; }
+
     }
 }
--- JobOffice.DataAcces/Entities/Category.cs
+++ JobOffice.DataAcces/Entities/Category.cs
@@ -8,14 +8,21 @@ namespace JobOffice.DataAcces.Entities
         [MaxLength(50)]
         [MinLength(5)]
         public string Name { get; set; }
+
         [MaxLength(50)]
         [MinLength(5)]
-        public string Description { get; set; }
-        public string Picture { get; set; }
-        public string CategoryURL { get; set; }
-        public int IdSubCategory { get; set; }
+        public string? Description { get; set; }  
+
+        public string? Picture { get; set; }  
+
+        public string? CategoryURL { get; set; }  
+
+        public int? IdSubCategory { get; set; }  
+
         public bool isActive { get; set; }
+
         public DateTime CreatedCategoryId { get; set; } = DateTime.Now;
-        public ICollection<SubCategory> SubCategories { get; set; }
+
+        public ICollection<SubCategory>? SubCategories { get; set; }  
     }
 }
