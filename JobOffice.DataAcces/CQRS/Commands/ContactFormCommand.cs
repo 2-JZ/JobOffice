8f8de41cbe2b1737c91f4dc66433d2499395b63c
2-JZ <tomaskowalcz@gmail.com>
2024-02-28 19:38:52 +0100

    adding components email

---
+++ JobOffice.DataAcces/CQRS/Commands/ContactFormCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobOffice.DataAcces.CQRS.Commands
+{
+    public class ContactFormCommand : IRequest<bool>
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
