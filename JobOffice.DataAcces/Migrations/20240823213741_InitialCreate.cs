3cc7ded98d0fac4ca7ca9bb90c583efe3adc069d
2-JZ <tomaskowalcz@gmail.com>
2024-08-23 23:56:50 +0200

    Changes after restoring blazor office

---
+++ JobOffice.DataAcces/Migrations/20240823213741_InitialCreate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobOffice.DataAcces.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Categories");
+        }
+    }
+}
